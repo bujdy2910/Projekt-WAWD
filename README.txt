7cad709c0cf9ce9327c9db85ef692ef9dada06b5
bujdy2910 <matobujdos@gmail.com>
2016-12-08 21:58:28 +0100

    final version

---
+++ README.txt
@@ -0,0 +1,2 @@
+Joko, spravil som vrchnú lištu v modrej farbe (pod¾a farby univerzity)
+Na ¾avú stranu chcem da ešte logo keï ho dokonèíme.
---
+++ index.php
@@ -0,0 +1,319 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<title>Eubáčik</title>
+	<meta name="description" content="Eubáčik">
+	<!-- Latest compiled and minified CSS update update update-->
+	 <!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+</head>
+
+	<style>
+	body{
+		padding-top: 40px;
+	}
+	</style>
+
+<body data-spy="scroll" data-target="#my-navbar">
+
+  <!-- Navbar -->
+  	<nav class="navbar navbar-toogle bg-primary navbar-fixed-top " id="my-navbar">
+  		<div class="container">
+  			<div class="navbar-header">
+  				<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse">
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  					<span class="icon-bar"></span>
+  				</button>
+  				<a class="navbar-brand"> <img src="assets/img/eubacik_logo.png"></a></div><!-- Navbar Header-->
+  			<div class="collapse navbar-collapse" id="navbar-collapse">
+  				
+  			<ul class="nav navbar-nav navbar-right">
+					<li><a href="#feedback" style="color: black">KDE NÁS NÁJDEŠ</a> 
+  					<li><a href="#program" style="color: black">PROGRAM</a> 
+  					<li><a href="#ceny" style="color: black">CENY LÍSTKOV</a> 
+  					<li><a href="#uni" style="color: black">O UNIVERZITE</a> 
+  					<li><a href="#contact" style="color: black">KONTAKT</a>
+  					<li><a href="https://www.facebook.com/eubacik/"><img src="http://aspyra.com/wp-content/uploads/facebook_icon-20x20.png"</a>
+  					 
+  				</ul>
+  			</div>
+  		</div><!-- End Container-->
+  	</nav><!-- End navbar -->
+  	 
+    	<!-- jumbotron-->
+<div class="jumbotron">
+<script src="js/jquery-1.11.3.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<div id="carousel1" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carousel1" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel1" data-slide-to="1"></li>
+    <li data-target="#carousel1" data-slide-to="2"></li>
+      </ol>
+ <div class="carousel-inner" role="listbox">
+    <div class="item active"><img src="assets/img/slide1.jpg" alt="First slide image" class="center-block">
+      <div class="carousel-caption">
+		  <h2><font color="white">Festival pre všetkých</font></h2>
+      </div>
+    </div>
+    <div class="item"><img src="assets/img/slide2.jpg" alt="Second slide image" class="center-block">
+      <div class="carousel-caption">
+		  <h2><font color="white">Pre tvrdých rockerov</font></h2>
+        
+      </div>
+    </div>
+    <div class="item"><img src="assets/img/slide3.jpg" alt="Third slide image" class="center-block">
+      <div class="carousel-caption">
+		  <h2><font color="white">Aj pre fanúšikov elektronickej hudby</font></h2>
+      </div>
+    </div>
+  </div>
+  <a class="left carousel-control" href="#carousel1" role="button" data-slide="prev"><span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="right carousel-control" href="#carousel1" role="button" data-slide="next"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span><span class="sr-only">Next</span></a></div>
+
+<!-- Contact -->
+
+  <div class="container">
+    <section>
+      <div class="page-header" id="feedback">
+          <h2>Kde nás nájdeš</h2>
+        </div><!-- End Page Header -->
+
+        <div class="row">
+          <div class="col-lg-4">
+			  </br></br>
+		<p><font color="black"></font>V prípade problémov nás môžeš nájsť:</font></p>
+
+
+            <address>
+              <strong>Festival Eubáčik</strong></br>
+              Dolnozemská cesta 1 </br>
+              Bratislava 82105</br>
+              Telefón 0919 191 919
+            </address>
+          </div>
+          
+          <div class="col-lg-8">
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2663.0867099673783!2d17.129739315648653!3d48.1278511792226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x476c891b3743e361%3A0x8ab5051e7f0cfc1f!2sDolnozemsk%C3%A1+cesta+2410%2F1%2C+851+04+Petr%C5%BEalka!5e0!3m2!1ssk!2ssk!4v1479163277250" width="400" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+        </div><!-- End the row -->
+
+    </section>
+  </div>
+        
+        <!--Kontakt-->
+        <hr>
+     <!-- Program -->
+	<div class="container">
+		<section>
+			<div class="page-header" id="program">
+  				<h2>Program</h2>
+  			</div><!-- End Page Header -->
+
+  			<div class="row">
+  				<div class="col-sm-8">
+  					<h3>Slovenká hudba</h3>
+  					<p>O program počas celého dňa sa budú starať tvoji obľúbení slovenskí interpréti. Kapely ako Horkýže Slíže, Iné Kafe, Polemic ti snáď už ani nemusíme predstavovať. Bavíme sa až do rána bieleho!</p>
+  				</div>
+  				<div class="col-sm-4">
+  					<img src="assets/img/horkyze.jpg" class="img-responsive" alt="image">
+  				</div>
+  			</div><!-- End row -->
+  			<div class="row">
+  				<div class="col-sm-8">
+  					<h3>FS Ekonóm</h3>
+  					<p>Poobedný program nám okorení univerzitný folklórny súbor- FS Ekonóm. Zabav sa na ľudovú nôtu na program študentov našej školy. Ak chceš, môžes sa zapísať a stať sa naším parťákom!</p>
+  				</div>
+  				<div class="col-sm-4">
+  					<img src="assets/img/ekonom.jpg" class="img-responsive" alt="image">
+  				</div>
+  			</div>
+  			<div class="row">
+  				<div class="col-sm-8">
+  					<h3>Diskusie</h3>
+  					<p>Počas celého konania festivalu sme si pripravili zaujímavé prednášky so zaujímavými osobnosťami-  s profesormi, so študentmi, s úspešnými absolventmi. Príd sa informovať.</p>
+  				</div>
+  				<div class="col-sm-4">
+  					<img src="assets/img/diskusia.jpg" class="img-responsive" alt="image">
+  				</div>
+  			</div>
+
+
+  					</section>
+	</div><!-- End Container -->   
+        
+        
+        <hr>
+        
+<!-- ceny -->
+	<div class="container">
+		<section>
+			<div class="page-header" id="ceny">
+  				<h2>Ceny</h2>
+  			</div><!-- End Page Header -->
+
+
+  			<div class="row">
+  				<div class="col-sm-4">
+  					<div class="panel panel-default text-center">
+  						<div class="panel-body">
+  							<!--<span class="glyphicon glyphicon-ok"></span>-->
+  							<span></span><h3> <img src="assets/img/dva.png"></h3></span>
+  							<h4>Záujemca</h4>
+  							<p>Ak nevieš čo po strednej, prídi si vyskúšať život na našej škole.</p>
+  						</div>
+  					</div>
+  				</div>
+
+  				<div class="col-sm-4">
+  					<div class="panel panel-default text-center">
+  						<div class="panel-body">
+  							<span></span><h3> <img src="assets/img/sest.png"></h3></span>
+  							<h4>Študent</h4>
+  							<p>Študenti našej univerzity sa bavia za lacno!</p>
+  						</div>
+  					</div>
+  				</div>
+
+  				<div class="col-sm-4">
+  					<div class="panel panel-default text-center">
+  						<div class="panel-body">
+  							<span></span><h3> <img src="assets/img/desat.png"></h3></span>
+  							<h4>Návštevník</h4>
+  							<p>Prídi sa zabaviť aj keď nie si náš študent.</p>
+  						</div>
+  					</div>
+  				</div>
+  			</div><!-- end row -->
+
+		</section>
+	</div><!-- End Container -->
+       <hr>
+        	<!-- O univerzite-->
+  	<div class="container">
+  		<section>
+  			<div class="page-header" id="uni">
+  				<h2>O univerzite</h2>
+  			</div>
+
+  			<div class="row">
+  				<div class="col-md-4">
+  					<blockquote>
+  						<p>Ekonomická univerzita v Bratislave predstavuje v súčasnosti najväčšiu univerzitu v SR, ktorá zabezpečuje na všetkých stupňoch štúdia komplexné a ucelené vzdelávanie v ekonomických a manažérskych študijných programoch. </p>
+						<a href="http://www.euba.sk"><button class="btn">Zisti viac</button></a>
+  					</blockquote>
+  				</div>
+  				<div class="col-md-4">
+  					<blockquote>
+  						<p>Slovenských zamestnávaťeľov aj tento rok najviac zaujímali životopisy absolventov Ekonomickej univerzity. Navyše absolventi až 5 naších fakúlt sa umietnili medzi najžiadajnejšími medzi zamestnávateľmi. Pridaj sa k úspšeným! </p>
+  						<a href="http://firma.profesia.sk/rebricek-vysokych-skol-slovensku-ekonomov-informatikov-technikov-vyhladavaju-zamestnavatelia/"><button class="btn">Zisti viac</button></a>
+  					</blockquote>
+  				</div>
+  				<div class="col-md-4">
+  					<blockquote>
+  						<p>Poskytujeme štúdium v 67 študijných programoch na 3 stupňoch štúdia. Prostredníctvom študentských mobilít získate možnosť stráviť časť štúdia na prestížnych partnerských univerzitách v zahraničí.</p>
+  						<a href="http://euba.sk/uchadzaci-o-studium/podmienky-prijatia-a-prijimacie-konanie"><button class="btn">Zisti viac</button></a>
+  					</blockquote>
+  				</div>
+  			</div><!-- End row -->
+  		</section>
+  	</div><!--End Container-->
+      
+      
+       <hr>
+       
+        <!-- CSS -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600">
+        <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/form-elements.css">
+        <link rel="stylesheet" href="assets/css/style.css">
+
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+        <!-- Favicon and touch icons -->
+        <link rel="shortcut icon" href="assets/ico/favicon.png">
+        <link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/ico/apple-touch-icon-144-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/ico/apple-touch-icon-114-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/ico/apple-touch-icon-72-precomposed.png">
+        <link rel="apple-touch-icon-precomposed" href="assets/ico/apple-touch-icon-57-precomposed.png">
+
+    </head>
+
+    <body>
+
+		<!-- Top content -->
+        <div class="top-content">
+        <div class="page-header" id="contact">
+        	
+            <div class="inner-bg">
+                <div class="container">
+                    <div class="row"> </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3 form-box">
+                        	<div class="form-top">
+                    			<h2>Kontaktuj nás</h2>
+                        		<p>Vylnením formulára nám odošleš správu:</p>
+                            </div>
+                            <div class="form-bottom contact-form">
+			                    <form role="form" action="assets/contact.php" method="post">
+			                    	<div class="form-group">
+			                    		<label class="sr-only" for="contact-email">Email</label>
+			                        	<input type="text" name="email" placeholder="Email..." class="contact-email form-control" id="contact-email">
+			                        </div>
+			                        <div class="form-group">
+			                        	<label class="sr-only" for="contact-subject">Subject</label>
+			                        	<input type="text" name="subject" placeholder="Subjekt..." class="contact-subject form-control" id="contact-subject">
+			                        </div>
+			                        <div class="form-group">
+			                        	<label class="sr-only" for="contact-message">Message</label>
+			                        	<textarea name="message" placeholder="Správa..." class="contact-message form-control" id="contact-message"></textarea>
+			                        </div>
+			                        <div class="form-group">
+			                        	<label for="contact-antispam">Antispam otázka: 7 + 5 = ?</label>
+			                        	<input type="text" name="antispam" placeholder="Vaša odpoveď..." class="contact-antispam form-control" id="contact-antispam">
+			                        </div>
+			                        <button type="submit" class="btn">Odoslať</button>
+			                    </form>
+		                    </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+
+        <!-- Javascript -->
+        <script src="assets/js/jquery-1.11.1.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery.backstretch.min.js"></script>
+        <script src="assets/js/scripts.js"></script>
+        
+        <!--[if lt IE 10]>
+            <script src="assets/js/placeholder.js"></script>
+        <![endif]-->
+      <script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-88257138-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
+    </body>
+
+</html>
